e88676bc3894455217f0d7cb66af929986f586d5
RaphaelGyory <xellessprl@gmail.com>
2016-02-03 18:19:11 +0100

    Travis configuration

--- install-centrifugo.sh
+++
@@ -1,4 +0,0 @@
-set -ex
-wget https://github.com/centrifugal/centrifugo/releases/download/v1.3.3/centrifugo-1.3.3-linux-amd64.zip
-tar -xzvf centrifugo-1.3.3-linux-amd64.zip
-sudo mv centrifugo /usr/bin/centrifugo
